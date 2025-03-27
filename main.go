592a7f5d192bc0b3101f0573cfb2b2253647b084
Michael-Ralph <MichaelR@praxima.com>
2025-03-27 08:29:26 +0200

    Add currency symbols

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ func main() {
 	var revenue, expenses, taxRate float64
 
 	// Get variables
-	fmt.Print("What is your total revenue: ")
+	fmt.Print("What is your total revenue: R")
 	fmt.Scan(&revenue)
-	fmt.Print("What is your total expenses: ")
+	fmt.Print("What is your total expenses: R")
 	fmt.Scan(&expenses)
 	fmt.Print("What is your tax rate: ")
 	fmt.Scan(&taxRate)
@@ -20,14 +20,14 @@ func main() {
 	// EBT (Earnings Before Tax) = Revenue - Operating Expenses - Interest Expenses
 	EBT := revenue - expenses
 	// Tax Amount = EBT × Tax Rate
-	taxAmount := EBT * taxRate
+	taxAmount := EBT * (taxRate / 100)
 	// Net Profit = Revenue - Operating Expenses - Interest Expenses - (EBT × Tax Rate)
 	netProfit := EBT - (taxAmount)
 	// Pretax Margin Ratio = EBT / Revenue * 100
 	PMR := (EBT / revenue) * 100
 
 	// Outputs
-	fmt.Printf("Earnings Before Tax: %.2f\n", EBT)
-	fmt.Printf("Net profit: %.2f\n", netProfit)
+	fmt.Printf("Earnings Before Tax: R%.2f\n", EBT)
+	fmt.Printf("Net profit: R%.2f\n", netProfit)
 	fmt.Printf("Pretax Margin Ratio: %.2f\n", PMR)
 }
