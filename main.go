b86d0a934128ce79311eedcc003ee7886a860b6c
Michael-Ralph <MichaelR@praxima.com>
2025-03-27 07:58:34 +0200

    Add initial calculations

--- main.go
+++ main.go
@@ -18,13 +18,16 @@ func main() {
 
 	// Calculations
 	// EBT (Earnings Before Tax) = Revenue - Operating Expenses - Interest Expenses
-
+	EBT := revenue - expenses
 	// Tax Amount = EBT × Tax Rate
-
+	taxAmount := EBT * taxRate
 	// Net Profit = Revenue - Operating Expenses - Interest Expenses - (EBT × Tax Rate)
-
+	netProfit := EBT - (taxAmount)
 	// Pretax Margin Ratio = EBT / Revenue * 100
+	PMR := (EBT / revenue) * 100
 
 	// Outputs
-
+	fmt.Printf("Earnings Before Tax: %.2f\n", EBT)
+	fmt.Printf("Net profit: %.2f\n", netProfit)
+	fmt.Printf("Pretax Margin Ratio: %.2f\n", PMR)
 }
